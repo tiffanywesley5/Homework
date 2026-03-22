4bc5b89f925fcdbb8db7d419aa1a7948a4cd3945
Tiffany Wesley <tiffanywesley@apples-MacBook-Air.local>
2026-03-21 21:55:49 -0500

    Add Week2 homework

---
+++ Week2/PizzaHomework.js
@@ -0,0 +1,13 @@
+let pizzaPlace = "Tiffany's Pizzeria"
+const numberofTopping = 5
+
+console.log(pizzaPlace, typeof pizzaPlace)
+console.log(numberofToppings, typeof numberofToppings)
+
+console.log(`${pizzaPlace} offers ${numberOfToppings} delicious toppings!`);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
